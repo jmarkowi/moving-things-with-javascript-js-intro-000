589eefa1fa5b8523f716ca138b704f936343e831
Jesse Markowitz <jess.markowitz@gmail.com>
2020-05-16 02:23:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var dodger = document.getElementById('dodger')
 function moveDodgerRight() {
   var rightNumbers = dodger.style.left.replace('px', '')
   var right = parseInt(rightNumbers, 10)
- 
+
   if (right < 360) {
     dodger.style.left = `${right + 1}px`
   }
