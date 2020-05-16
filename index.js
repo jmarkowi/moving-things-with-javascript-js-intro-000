20bcdfc8fb82a44caae3b1e4d3424ad0300593cd
Jesse Markowitz <jess.markowitz@gmail.com>
2020-05-16 02:23:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,13 +3,19 @@ const app = "I don't do much."
 var dodger = document.getElementById('dodger')
 
 function moveDodgerRight() {
-  var rightNumbers = dodger.style.right.replace('px', '')
+  var rightNumbers = dodger.style.left.replace('px', '')
   var right = parseInt(rightNumbers, 10)
-
-  if (right > 0) {
-    dodger.style.right = `${right - 1}px`
+ 
+  if (right < 360) {
+    dodger.style.left = `${right + 1}px`
   }
 }
+
+document.addEventListener('keydown', function(e) {
+  if (e.which === 39) {
+    moveDodgerRight()
+  }
+})
 
 function moveDodgerLeft() {
   var leftNumbers = dodger.style.left.replace('px', '')
@@ -23,11 +29,5 @@ function moveDodgerLeft() {
 document.addEventListener('keydown', function(e) {
   if (e.which === 37) {
     moveDodgerLeft()
-  }
-})
-
-document.addEventListener('keydown', function(e) {
-  if (e.which === 39) {
-    moveDodgerRight()
   }
 })
